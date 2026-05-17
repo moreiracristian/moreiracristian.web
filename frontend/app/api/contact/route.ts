518a45be25cb1e88b9d39635c5a8b1428b46c8f5
moreiracristian <moreiracristian@hotmail.com>
2026-05-17 04:49:32 -0300

    feat: notificaciones por email, fix mobile y cambio de correo de contacto

--- frontend/app/api/contact/route.ts
+++ frontend/app/api/contact/route.ts
@@ -2,8 +2,6 @@ import { sql } from "@vercel/postgres";
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
   const { name, email, company, message } = await req.json();
   if (!name || !email || !message) {
@@ -18,6 +16,7 @@ export async function POST(req: Request) {
   `;
 
   if (process.env.RESEND_API_KEY) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     await resend.emails.send({
       from: "Sitio Web <onboarding@resend.dev>",
       to: "bymoreiracristian@gmail.com",
